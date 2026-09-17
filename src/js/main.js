bb51fe96702afc6c4da9d169ba10fc1ddfa5b7c6
simeon-obianwa <simeonobianwa@gmail.com>
2026-09-17 01:05:15 +0100

    Add customizable Alert module to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.js";
 import { renderCartCount } from "./utils.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const alert = new Alert();
+
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 
