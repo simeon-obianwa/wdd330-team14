524894bb564eba9e86d4367de0b1e278c555c04b
Kaylee Godfrey <kayrand9@gmail.com>
2026-09-24 08:31:23 -0500

    Fixed issue

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,20 +1,20 @@
-import { loadHeaderFooter } from "./utils.mjs";
-import CheckoutProcess from "./CheckoutProcess.mjs";
+import { loadHeaderFooter } from './utils.mjs';
+import CheckoutProcess from './CheckoutProcess.mjs';
 
 loadHeaderFooter();
 
-const myCheckout = new CheckoutProcess("so-cart", ".checkout");
+const myCheckout = new CheckoutProcess('so-cart', '.checkout');
 myCheckout.init();
 
 // calculate tax, shipping, and total once the user enters a zip code
-document.querySelector("#zip").addEventListener("blur", () => {
+document.querySelector('#zip').addEventListener('blur', () => {
   myCheckout.calculateOrderTotal();
 });
 
 // handle the form submission
 document
-  .querySelector("#checkoutForm")
-  .addEventListener("submit", async (e) => {
+  .querySelector('#checkoutForm')
+  .addEventListener('submit', async (e) => {
     e.preventDefault();
     const form = e.target;
     // browser validation: only proceed if all required fields are filled
@@ -22,4 +22,4 @@ document
       const response = await myCheckout.checkout(form);
       console.log(response);
     }
-  });
+      });
